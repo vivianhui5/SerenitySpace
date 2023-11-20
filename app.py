871dab569b4240014077bba9e53c7786d8963258
Vivian Hui <vivianhui05@gmail.com>
2023-11-19 19:37:35 -0500

    calming page works!

--- app.py
+++ app.py
@@ -6,5 +6,10 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/calming-exercises')
+def calming_exercises():
+    return render_template('calming-exercises.html')
+
+
 if __name__ == '__main__':
     app.run(debug=True)
