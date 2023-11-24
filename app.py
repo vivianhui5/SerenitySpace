cb3bb9e3d57fa683f5a1ee896d1a0371146bc959
Vivian Hui <vivianhui05@gmail.com>
2023-11-23 20:41:53 -0500

    bare bones journaling

--- app.py
+++ app.py
@@ -1,6 +1,29 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+from flask import redirect, url_for
+
 
 app = Flask(__name__)
+
+# Configure your database URI
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+db = SQLAlchemy(app)
+
+
+
+class JournalEntry(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    entry = db.Column(db.Text)
+
+    
+with app.app_context():
+    db.create_all()
+
+
+
+
+
+
 
 @app.route('/')
 def index():
@@ -33,6 +56,22 @@ def journaling():
 @app.route('/doodling')
 def doodling():
     return render_template('doodling.html')
+
+@app.route('/submit_journal', methods=['POST'])
+def submit_journal():
+    entry_text = request.form['journalEntry']
+    new_entry = JournalEntry(entry=entry_text)
+    db.session.add(new_entry)
+    db.session.commit()
+
+    return redirect(url_for('journal_board'))
+
+@app.route('/journal-board')
+def journal_board():
+    all_entries = JournalEntry.query.all()
+    return render_template('journal-board.html', entries=all_entries)
+
+
 
 
 if __name__ == '__main__':
